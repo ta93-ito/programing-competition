a57a16c66bc82fc277629ae3747efc7d1301c388
ta93-ito <tkm.ito@outlook.jp>
2022-01-15 01:13:30 +0900

    heap

--- heap/heap.rb
+++ heap/heap.rb
@@ -19,6 +19,40 @@ class Heap
     end
     heap[i] = vertex
   end
+
+  def top
+    heap.first
+  end
+
+  # 最大値を削除する
+  def pop
+    return if heap.empty?
+
+    # 先頭の要素を削除し末尾要素を先頭に移動させる。
+    heap.shift
+    last_child = heap.pop
+    heap.unshift(last_child)
+
+    # 値の大きい子とlast_childを比較し、last_childが子よりも小さくなるまで移動していく。
+    # 交換の際は子を上にスライドしていき、交換しないタイミングでlast_childをその位置に挿入する。
+    i = 0
+    while 2 * i + 1 < (heap.size - 1)
+      larger_index = larger_child_index(i)
+      break if heap[larger_index] <= last_child
+
+      heap[i] = heap[larger_index]
+      i = larger_index
+    end
+    heap[i] = last_child
+  end
+
+  def larger_child_index(i)
+    if heap[2 * i + 1] > heap[2 * i + 2]
+      2 * i + 1
+    else
+      2 * i + 2
+    end
+  end
 end
 
 heap = Heap.new
@@ -28,4 +62,7 @@ heap.push(3)
 heap.push(5)
 heap.push(2)
 heap.push(10)
-p heap.heap
+
+p "before: #{heap.heap}"
+heap.pop
+p "after: #{heap.heap}"
